8d99d7117ce4bd41798925709370fa2085459b56
Ezekiel Bethel <mctinfoilball@gmail.com>
2017-02-21 20:22:38 +0000

    debugger moved to thread - currently fails to create thread

--- rosalina/include/minisoc.h
+++ rosalina/include/minisoc.h
@@ -26,3 +26,4 @@ Result socPoll(struct pollfd *fds, nfds_t nfds, int timeout);
 int socClose(Handle sockfd);
 
 ssize_t soc_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+ssize_t soc_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
--- rosalina/source/menus/debugger.c
+++ rosalina/source/menus/debugger.c
@@ -13,7 +13,22 @@ Menu menu_debugger = {
     }
 };
 
+#define MAX_CLIENTS 8
+
 static bool debugger_enabled;
+static MyThread debuggerThread;
+static u8 ALIGN(8) debuggerThreadStack[THREAD_STACK_SIZE];
+
+void debuggerThreadMain(void);
+MyThread debuggerCreateThread(void)
+{
+    Result res = MyThread_Create(&debuggerThread, debuggerThreadMain, debuggerThreadStack, THREAD_STACK_SIZE, 1, CORE_SYSTEM);
+    char msg2[] = "00000000 threadCreate";
+    hexItoa(res, msg2, 8, false);
+    draw_string(msg2, 10, 30, COLOR_WHITE);
+
+    return debuggerThread;
+}
 
 void Debugger_Enable(void)
 {
@@ -29,10 +44,10 @@ void Debugger_Enable(void)
         draw_string("Initialising SOC...", 10, 10, COLOR_WHITE);
 
         s64 amt = 0;
-        svcGetSystemInfo(&amt, 0, 3);
+        svcGetSystemInfo(&amt, 0, 2);
 
-        char msg1[] = "00000000 free";
-        hexItoa(0x01400000 - amt, msg1, 8, false);
+        char msg1[] = "00000000 free in SYSTEM";
+        hexItoa(0x02C00000 - amt, msg1, 8, false);
         draw_string(msg1, 10, 20, COLOR_WHITE);
 
         Result res = miniSocInit(0x20000);
@@ -45,74 +60,9 @@ void Debugger_Enable(void)
         else
         {
             draw_string("SOC init success", 10, 10, COLOR_TITLE);
-
-            Handle sock = 0;
-
-            res = socSocket(&sock, AF_INET, SOCK_STREAM, 0);
-            while(R_FAILED(res))
-            {
-                draw_string("Sleeping...", 10, 20, COLOR_TITLE);
-                draw_flushFramebuffer();
-                svcSleepThread(100000000LL);
-
-                res = socSocket(&sock, AF_INET, SOCK_STREAM, 0);
-            }
-
-            draw_string("!!!!!", 10, 20, COLOR_TITLE);
-            draw_flushFramebuffer();
-
-            if(R_SUCCEEDED(res))
-            {
-                struct sockaddr_in saddr;
-                saddr.sin_family = AF_INET;
-                saddr.sin_port = 0xa00f; // 4000, byteswapped
-                saddr.sin_addr.s_addr = gethostid();
-
-                res = socBind(sock, (struct sockaddr*)&saddr, sizeof(struct sockaddr_in));
-
-                if(R_SUCCEEDED(res))
-                {
-                    res = socListen(sock, 2);
-                    if(R_SUCCEEDED(res))
-                    {
-                        draw_string("Accepting..", 10, 20, COLOR_TITLE);
-                        Handle client_sock = 0;
-                        res = socAccept(sock, &client_sock, NULL, 0);
-
-                        if(R_SUCCEEDED(res))
-                        {
-                            draw_string("Got client!", 10, 20, COLOR_TITLE);
-
-                            char buf[5];
-                            buf[4] = 0;
-                            res = soc_recvfrom(client_sock, buf, 4, 0, NULL, 0);
-                            if(R_SUCCEEDED(res))
-                            {
-                                draw_string(buf, 10, 30, COLOR_TITLE);
-                            }
-                            else
-                            {
-                                draw_string("recv fail :(", 10, 30, COLOR_WHITE);
-                            }
-
-                            socClose(client_sock);
-                            socClose(sock);
-                        }
-                        else
-                        {
-                            draw_string("accept fail :(", 10, 20, COLOR_WHITE);
-                        }
-                    }
-                    else
-                    {
-                        draw_string("listen fail :(", 10, 20, COLOR_WHITE);
-                    }
-                }
-                else
-                {
-                    draw_string("bind fail :(", 10, 20, COLOR_WHITE);
-                }
-            }
+            //debuggerThreadMain();
+            debuggerCreateThread();
+            MyThread_Join(&debuggerThread, 0);
         }
     }
 
@@ -137,4 +87,102 @@ void Debugger_Disable(void)
     draw_flushFramebuffer();
 
     while(!(waitInput() & BUTTON_B));
+}
+
+void debuggerThreadMain(void)
+{
+    draw_clearFramebuffer();
+    draw_flushFramebuffer();
+    draw_string("THREAD", 10, 10, COLOR_TITLE);
+    draw_flushFramebuffer();
+
+    bool stop = false;
+
+    Result res = 0;
+    Handle sock = 0;
+
+    res = socSocket(&sock, AF_INET, SOCK_STREAM, 0);
+    while(R_FAILED(res))
+    {
+        svcSleepThread(100000000LL);
+
+        res = socSocket(&sock, AF_INET, SOCK_STREAM, 0);
+    }
+
+    if(R_SUCCEEDED(res))
+    {
+        struct sockaddr_in saddr;
+        saddr.sin_family = AF_INET;
+        saddr.sin_port = 0xa00f; // 4000, byteswapped
+        saddr.sin_addr.s_addr = gethostid();
+
+        res = socBind(sock, (struct sockaddr*)&saddr, sizeof(struct sockaddr_in));
+
+        if(R_SUCCEEDED(res))
+        {
+            res = socListen(sock, 2);
+            if(R_SUCCEEDED(res))
+            {
+                struct pollfd fds[8];
+                nfds_t nfds = 1;
+                fds[0].fd = sock;
+                fds[0].events = POLLIN;
+
+                while(!stop)
+                {
+                    int res = socPoll(fds, nfds, -1);
+                    for(unsigned int i = 0; i < nfds; i++)
+                    {
+                        if((fds[i].revents & POLLIN) == POLLIN)
+                        {
+                            if(i == 0)
+                            {
+                                Handle client_sock = 0;
+                                res = socAccept(sock, &client_sock, NULL, 0);
+                                if(nfds == MAX_CLIENTS)
+                                {
+                                    socClose(client_sock);
+                                }
+                                fds[nfds].fd = client_sock;
+                                fds[nfds].events = POLLIN;
+                                nfds++;
+                            }
+                            else
+                            {
+                                char buf[6];
+                                buf[5] = 0;
+                                res = soc_recvfrom(fds[i].fd, buf, 5, 0, NULL, 0);
+                                if(R_SUCCEEDED(res))
+                                {
+                                    draw_string(buf, 10, 30, COLOR_TITLE);
+                                    soc_sendto(fds[i].fd, buf, 5, 0, NULL, 0);
+                                    if(memcmp(buf, "stop", 4) == 0)
+                                    {
+                                        stop = true;
+                                    }
+                                }
+                                else
+                                {
+                                    //draw_string("recv fail :(", 10, 30, COLOR_WHITE);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for(unsigned int i = 0; i < nfds; i++)
+                {
+                    socClose(fds[i].fd);
+                }
+            }
+            else
+            {
+                //draw_string("listen fail :(", 10, 20, COLOR_WHITE);
+            }
+        }
+        else
+        {
+            //draw_string("bind fail :(", 10, 20, COLOR_WHITE);
+        }
+    }
 }
