4510e065f5eb940699d5ae0ba4789d41a1cf0e31
TuxSH <tuxsh@sfr.fr>
2017-02-24 01:14:18 +0100

    Fix OOB read bug

--- rosalina/kernel_extension/source/svc/GetProcessInfo.c
+++ rosalina/kernel_extension/source/svc/GetProcessInfo.c
@@ -6,7 +6,7 @@ Result GetProcessInfoHook(u32 dummy UNUSED, Handle processHandle, u32 type)
     u64 out = 0; // should be s64 but who cares
     Result res = 0;
 
-    if(type == 0x10000 || type == 0x10001)
+    if(type >= 0x10000)
     {
         KProcessHandleTable *handleTable = handleTableOfProcess(currentCoreContext->objectContext.currentProcess);
         KProcess *process = KProcessHandleTable__ToKProcess(handleTable, processHandle);
@@ -14,11 +14,27 @@ Result GetProcessInfoHook(u32 dummy UNUSED, Handle processHandle, u32 type)
         if(process == NULL)
             res = 0xD8E007F7; // invalid handle
 
-        else if(type == 0x10000)
-            memcpy(&out, codeSetOfProcess(process)->processName, 8);
-
-        else
-            out = codeSetOfProcess(process)->titleId;
+        switch(type)
+        {
+            case 0x10000:
+                memcpy(&out, codeSetOfProcess(process)->processName, 8);
+                break;
+            case 0x10001:
+                out = codeSetOfProcess(process)->titleId;
+                break;
+            case 0x10002:
+                out = codeSetOfProcess(process)->nbTextPages << 12;
+                break;
+            case 0x10003:
+                out = codeSetOfProcess(process)->nbRodataPages << 12;
+                break;
+            case 0x10004:
+                out = codeSetOfProcess(process)->nbRwPages << 12;
+                break;
+            default:
+                res = 0xD8E007ED; // invalid enum value
+                break;
+        }
 
         KAutoObject *obj = (KAutoObject *)process;
         if(process != NULL)
