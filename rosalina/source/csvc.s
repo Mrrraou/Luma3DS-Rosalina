77af3a3c8a44a0cfb2cb72391e6769a92e305e11
TuxSH <tuxsh@sfr.fr>
2017-04-11 23:56:05 +0200

    Add CFI for custom SVCs

--- rosalina/source/csvc.s
+++ rosalina/source/csvc.s
@@ -2,16 +2,22 @@
 .balign 4
 
 .macro SVC_BEGIN name
-	.section .text.\name, "ax", %progbits
-	.global \name
-	.type \name, %function
-	.align 2
+    .section .text.\name, "ax", %progbits
+    .global \name
+    .type \name, %function
+    .align 2
+    .cfi_startproc
 \name:
+.endm
+
+.macro SVC_END
+    .cfi_endproc
 .endm
 
 SVC_BEGIN svcCustomBackdoor
     svc 0x80
     bx lr
+SVC_END
 
 SVC_BEGIN svcConvertVAToPA
     str r0, [sp, #-4]!
@@ -20,23 +26,29 @@ SVC_BEGIN svcConvertVAToPA
     cmp r2, #0
     strne r1, [r2]
     bx lr
+SVC_END
 
 SVC_BEGIN svcFlushDataCacheRange
     svc 0x82
     bx lr
+SVC_END
 
 SVC_BEGIN svcFlushEntireDataCache
     svc 0x83
     bx lr
+SVC_END
 
 SVC_BEGIN svcInvalidateInstructionCacheRange
     svc 0x84
     bx lr
+SVC_END
 
 SVC_BEGIN svcInvalidateEntireInstructionCache
     svc 0x85
     bx lr
+SVC_END
 
 SVC_BEGIN svcMapProcessMemoryWithSource
     svc 0x86
     bx lr
+SVC_END
