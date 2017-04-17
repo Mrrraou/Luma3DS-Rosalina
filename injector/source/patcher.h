58f8eed2de4a0415144cd5c368948019ab111067
Aurora Wright <davidone92@katamail.com>
2017-04-17 02:14:17 +0200

    Make loader search for patterns in just the appropriate code.bin segment, tentatively fix compatibility with Daigasso updates
    Conflicts:
    	injector/source/patcher.c

--- injector/source/patcher.h
+++ injector/source/patcher.h
@@ -36,4 +36,4 @@ enum singleOptions
     PATCHUNITINFO
 };
 
-void patchCode(u64 progId, u16 progVer, u8 *code, u32 size, u32 textSize);
+void patchCode(u64 progId, u16 progVer, u8 *code, u32 size, u32 textSize, u32 roSize);
